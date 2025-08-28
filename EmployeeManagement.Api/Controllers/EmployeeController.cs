39de0130ca0fd5e1e7316d19bff9a62ae2a1bc45
Rinsu Pradhan <rinsupradhan@gmail.com>
2025-08-28 15:35:07 +0545

    feat(employee): implement AddEmployee feature

---
+++ EmployeeManagement.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.Domain.Entities;
+using EmployeeManagement.Application.Commands;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace EmployeeManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController(ISender sender) : ControllerBase
+    {
+        [HttpPost("")]
+        //From body means Json Body
+        public async Task<IActionResult> AddEmployeeAsync([FromBody] Employee employee)
+        {
+            
+            
+            var result = await sender.Send(new AddEmployeeCommand(employee));
+
+            return Ok(result);
+        }
+    }
+}
--- EmployeeManagement.Application/DependencyInjection.cs
+++ EmployeeManagement.Application/DependencyInjection.cs
@@ -11,6 +11,8 @@ namespace EmployeeManagement.Application
     {
         public static IServiceCollection AddApplicationDI(this IServiceCollection services)
         {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
             // Register application services here
             return services;
         }
